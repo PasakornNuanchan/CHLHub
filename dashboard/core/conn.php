e9768ea09965090627678e353d4e732a37c695dc
ATHENA\art44 <Blackbetty4475@gmail.com>
2022-10-25 15:29:12 +0700

    art

---
+++ dashboard/core/conn.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name = "chl_uat";
+
+
+$con=mysqli_connect($host,$user,$pass ,$db_name);
+
+?>
---
+++ dashboard/core/theme_include.php
@@ -0,0 +1,51 @@
+    <!-- Title Icon -->
+    <link rel="shortcut icon" href="../../assets/images/favicon.ico" />
+      
+    <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="../../assets/css/core/libs.min.css" />
+    
+    
+    <!-- Hope Ui Design System Css -->
+    <link rel="stylesheet" href="../../assets/css/hope-ui.min.css?v=1.2.0" />
+    
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="../../assets/css/custom.min.css?v=1.2.0" />
+    
+    <!-- Dark Css -->
+    <link rel="stylesheet" href="../../assets/css/dark.min.css"/>
+    
+    <!-- Customizer Css -->
+    <link rel="stylesheet" href="../../assets/css/customizer.min.css" />
+    
+    <!-- RTL Css -->
+    <link rel="stylesheet" href="../../assets/css/rtl.min.css"/>
+    
+    <!-- Library Bundle Script -->
+    <script src="../../assets/js/core/libs.min.js"></script>
+    
+    <!-- External Library Bundle Script -->
+    <script src="../../assets/js/core/external.min.js"></script>
+    
+    <!-- Widgetchart Script -->
+    <script src="../../assets/js/charts/widgetcharts.js"></script>
+    
+    <!-- mapchart Script -->
+    <script src="../../assets/js/charts/vectore-chart.js"></script>
+    <script src="../../assets/js/charts/dashboard.js" ></script>
+    
+    <!-- fslightbox Script -->
+    <script src="../../assets/js/plugins/fslightbox.js"></script>
+    
+    <!-- Settings Script -->
+    <script src="../../assets/js/plugins/setting.js"></script>
+    
+    <!-- Slider-tab Script -->
+    <script src="../../assets/js/plugins/slider-tabs.js"></script>
+    
+    <!-- Form Wizard Script -->
+    <script src="../../assets/js/plugins/form-wizard.js"></script>
+    
+    <!-- AOS Animation Plugin-->
+    
+    <!-- App Script -->
+    <script src="../../assets/js/hope-ui.js" defer></script>
