890a84cd3a440ac14d501e0a5d0bb12255f284af
Mnmanomai <mnmanomai@gmail.com>
2022-11-14 11:18:35 +0700

    Update nevbarmain.php

--- dashboard/include/nevbarmain.php
+++ dashboard/include/nevbarmain.php
@@ -272,14 +272,14 @@
         </div>
     </div>
 </nav>
-<div class="iq-navbar-header" style="height: 215px;">
+<div class="iq-navbar-header" style="height: 140px;">
     <div class="container-fluid iq-container">
         <div class="row">
             <div class="col-md-12">
                 <div class="flex-wrap d-flex justify-content-between align-items-center">
                     <div>
                         <h1>Hello Devs!</h1>
-                        <p>We are on a mission to help developers like you build successful projects for FREE.</p>
+                        
                     </div>
                     <div>
                         <a href="" class="btn btn-link btn-soft-light">
