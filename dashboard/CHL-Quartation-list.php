5819022fec9118557debd4e36284f11eef1f1587
Mnmanomai <mnmanomai@gmail.com>
2023-03-31 10:12:55 +0700

    Quotation list
    เปลี่ยนจาก query ajax จาก query ใน หน้า php

--- dashboard/CHL-Quartation-list.php
+++ dashboard/CHL-Quartation-list.php
@@ -1,6 +1,6 @@
 <?php
 require 'function/auth/get_session.php';
- 
+
 ?>
 <!doctype html>
 <html lang="en" dir="ltr">
@@ -33,28 +33,21 @@ require 'function/auth/get_session.php';
         </div>
         <div class="conatiner-fluid content-inner mt-n5 py-0">
             <!-- MAIN BODY START -->
-            <div class="row">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bcpage">
-                        <li class="breadcrumb-item"><a href="" style="color:white;">Quotation List Main list </a></li>
-                    </ol>
-                </nav>
-            </div>
-
+        
             <div class="row ">
                 <div class="col-sm-12">
                     <div class="card">
                         <div class="card-body">
                             <div class="form-group row">
-                                <label class="control-label col-sm-2 col-lg-2 align-self-center mb-0" for="pwd2">Quotation status:</label>
+                                <!-- <label class="control-label col-sm-2 col-lg-2 align-self-center mb-0" for="pwd2">Quotation status:</label> -->
                                 <div class="col">
                                     <div class="row">
                                         <div class="col-lg-3">
-                                            <select name="" id="" class="form form-select form-select-sm">
+                                            <!-- <select name="" id="" class="form form-select form-select-sm">
                                                 <option value="">All</option>
                                                 <option value="">Sign</option>
                                                 <option value="">Not Sign</option>
-                                            </select>
+                                            </select> -->
                                         </div>
                                         <div class="col-lg-7"></div>
                                         <div class="col-lg-2">
@@ -64,37 +57,66 @@ require 'function/auth/get_session.php';
                                 </div>
                             </div>
                             <div class="bd-example table-responsive">
-                            <table id="datatable" class="table table-hover" name="data_table_list" data-toggle="data-table" style="border-radius: 12px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
-                                <thead>
-                                    <tr class="text-center bg-gradient" style="background-color :#0D47A1; color :aliceblue;">
-                                        <th>Create Date</th>
-                                        <th>Quotation number</th>
-                                        <th>Sale</th>
-                                        <th>Consignee</th>
-                                        <th>Quotation Type</th>
-                                        <th>Status</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody align="center">
-                                        <tr>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
+                                <table id="datatable" class="table table-hover" name="data_table_list" data-toggle="data-table" style="border-radius: 12px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
+                                    <thead>
+                                        <tr class="text-center bg-gradient" style="background-color :#0D47A1; color :aliceblue;">
+                                            <th>Create Date</th>
+                                            <th>Quotation number</th>
+                                            <th>Sale</th>
+                                            <th>Consignee</th>
+                                            <th>Quotation Type</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
                                         </tr>
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody align="center">
+
+                                        <?php
+                                        $sql_query = "
+                                SELECT 
+                                    qt.create_datetime,
+                                    qt.quartation_number,
+                                    sale.first_name,
+                                    sale.last_name,
+                                    c.consignee_name,
+                                    qt.type,
+                                    qt.status
+                                FROM 
+                                quartation_title as qt
+                                INNER JOIN user as sale ON qt.user_sale = sale.user_number
+                                INNER JOIN consignee as c ON qt.consignee_number = c.consignee_number
+                                ";
+
+                                        $fetch_sql = mysqli_query($con, $sql_query);
+                                        while ($result_table_list = mysqli_fetch_assoc($fetch_sql)) {
+                                        if($result_table_list['status'] == 1) {
+                                            $status_quo_list = '<span class="badge rounded-pill bg-success" style="border-radius: 12px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">sign</span>';
+                                        } else {
+                                            $status_quo_list = '<span class="badge rounded-pill bg-danger" style="border-radius: 12px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">unsign</span>';
+                                        }
+
+                                        ?>
+                                            <tr>
+                                                <td><?= $result_table_list['create_datetime']; ?></td>
+                                                <td><?= $result_table_list['quartation_number']; ?></td>
+                                                <td><?= $result_table_list['first_name']; ?> <?= $result_table_list['last_name']; ?></td>
+                                                <td><?= $result_table_list['consignee_name']; ?></td>
+                                                <td><?= $result_table_list['type']; ?></td>
+                                                <td><?= $status_quo_list ?></td>
+                                                <td><button type="button" onclick="quartation_list.preview('<?= $result_table_list['quartation_number']; ?>');" class="btn btn-primary rounded-pill btn-sm bg-gradient" style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"><i class="bi bi-eye"></i> Preview</button></td>
+                                            </tr>
+                                        <?php
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            </div>
-       
-        
+
+
 
 
             <!-- MAIN BODY END -->
@@ -116,7 +138,7 @@ require 'function/auth/get_session.php';
 <script src="js/quotation-list/quotation_list.js"></script>
 <script src="js/quotation-list/quotation_list_set.js"></script>
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         sidebar_main.set_data_rows();
         quotation_list_set.set_data_rows();
     });
--- dashboard/js/quotation-list/quotation_list_set.js
+++ dashboard/js/quotation-list/quotation_list_set.js
@@ -1,58 +1,14 @@
 const quotation_list_set = {
    
-    set_data_rows: async function (job_doc_pt) {
+    set_data_rows: async function () {
         
         $('.head-of-menu').html('Quotation List');
-        let res_data = await quotation_list_set.ajax_set_preview_data(job_doc_pt);
+        $('.bcpage').html('');
+        html_bdpage = `
+        <li class="breadcrumb-item"><a href="CHL-Quartation-list.php" target="" style="color:white;">Quotation Main List</a></li>`;
+        $('.bcpage').append(html_bdpage);
         
-        console.log(res_data);
-        $('[name = "data_table_list"] tbody').html('');
-
-        $.each(res_data['quotation_list'], function (i, v) {         
-            if(v['status']==1){
-                status_quo_list = '<span class="badge rounded-pill bg-success" style="border-radius: 12px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">sign</span>';
-            }else{
-                status_quo_list = '<span class="badge rounded-pill bg-danger" style="border-radius: 12px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">unsign</span>';
-
-            }
-            sub_date_time = v['create_datetime'];
-            res_date_time = sub_date_time.substring(0, 10);
-            
-            
-            html_set_pct = `
-            <tr>
-            <td>${res_date_time}</td>
-            <td>${v['quartation_number']}</td>
-            <td>${v['first_name']} ${v['last_name']}</td>
-            <td>${v['consignee_name']}</td>
-            <td>${v['type']}</td>
-            <td>${status_quo_list}</td>
-            <td><button type="button" onclick="quartation_list.preview('${v['quartation_number']}');" class="btn btn-primary rounded-pill btn-sm bg-gradient" style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"><i class="bi bi-eye"></i> Preview</button></td>
-        </tr>
-            `;
-
-            $('[name = "data_table_list"] tbody').append(html_set_pct);
-           
-        });
-      
-      
     }, 
-    
-
-    ajax_set_preview_data: function (job_doc_pt) {
-        
-        return new Promise(function (resolve, reject) {
-            $.ajax({
-                type: "post",
-                url: "php/quotation-list/get_detail.php",
-                data: {},
-                dataType: "json",
-                success: function (response) {
-                    resolve(response);
-                }
-            });
-        });
-    },
 
 };
 
